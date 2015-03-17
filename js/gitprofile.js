d9b79cdac41a4cbe177c58c42a3bdabcc16d77d1
Erik Griffin <griffin.erik@gmail.com>
2015-03-17 15:39:41 +0000

    Added detailed error message

--- js/gitprofile.js
+++ js/gitprofile.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 
     event.preventDefault();
 
-
     var username = $('.username').val();
     var url = 'http://api.github.com/users/'+username;
     var template = $('template').html();
@@ -14,20 +13,13 @@ $(document).ready(function() {
       type: "GET",
       dataType: 'json',
       success: function(response){
-        console.log(response);
-
-        var test = Mustache.render(template, response)
-        console.log(test)
-        $('main').append(test);
-
-
-
-
-
+        $('main').append(Mustache.render(template,response));
+        $('.system-message').text('');
       },
-      error: function(){
+      error: function(error){
 
-        console.log("error!");
+        console.log("error: "+error.responseJSON.message);
+        $('.system-message').text(error.responseJSON.message);
       }
     })
     .always(function() {
